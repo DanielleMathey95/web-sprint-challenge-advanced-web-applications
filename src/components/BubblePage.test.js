0c734f573eb835a24490ffc0554f9b265685531c
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-08-15 16:59:33 -0700

    BubblePage.test.js has both tests passing

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
 
-import { render, screen} from "@testing-library/react";
-import BubblePage from './BubblePage';
+import { render, screen } from "@testing-library/react";
+import BubblePage from "./BubblePage";
 
-
-test("Renders without errors", ()=> {
-    render(<BubblePage />)
+test("Renders without errors", () => {
+  render(<BubblePage />);
 });
 
-test("Renders appropriate number of colors passed in through mock", async ()=> {
-    //Keep in mind that our service is called on mount for this
-    render(<BubblePage />);
-    const colors = screen.getByText(/colors/i);
-    expect(colors).toBeInTheDocument();
+test("Renders appropriate number of colors passed in through mock", async () => {
+  //Keep in mind that our service is called on mount for this
+  render(<BubblePage />);
+  const colors = screen.getByText(/colors/i);
+  expect(colors).toBeInTheDocument();
 });
